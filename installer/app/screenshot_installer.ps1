a670d70c8bd517ee3dd4c7372cf42cd557eaf2b8
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2023-03-10 22:08:58 +0700

    Update screenshot_installer.ps1

--- installer/app/screenshot_installer.ps1
+++ installer/app/screenshot_installer.ps1
@@ -1,14 +1,15 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+# Set-ExecutionPolicy Bypass -Scope Process -Force;
+# iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 
-scoop install git
-scoop bucket add extras
-scoop bucket add main
+# scoop install git
+# scoop bucket add extras
+# scoop bucket add main
 
-# vim
-scoop install vim
+# # vim
+# scoop install vim
 
-scoop install python
+# scoop install python
 
+choco install python
 pip install Pillow
 pip install pywin32
