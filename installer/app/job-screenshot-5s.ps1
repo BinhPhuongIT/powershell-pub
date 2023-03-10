c70dc218787248365de3e07709ceee54649031a3
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2023-03-10 22:54:27 +0700

    Update job-screenshot-5s.ps1

--- installer/app/job-screenshot-5s.ps1
+++ installer/app/job-screenshot-5s.ps1
@@ -15,3 +15,6 @@ Register-ScheduledTask -TaskName "Screenshot" -Action $action -Trigger $trigger 
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(5) -RepetitionInterval (New-TimeSpan -Seconds 5) -RepetitionDuration ([TimeSpan]::MaxValue)
 $action = New-ScheduledTaskAction -Execute "python.exe" -Argument "-ExecutionPolicy Bypass -File C:\Program Files\screenshot.py"
 Register-ScheduledTask -TaskName "ScreenshotJob" -Trigger $trigger -Action $action -RunLevel Highest -User "administrator"
+
+
+Register-ScheduledJob -Name "ScreenshotJob" -ScriptBlock {python.exe 'C:\Program Files\screenshot.py'} -Trigger (New-JobTrigger -AtStartup -RandomDelay 00:00:30)
