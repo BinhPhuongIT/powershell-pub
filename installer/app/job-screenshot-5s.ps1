bc252465d376bfe85ea8f3c09dda3d8aefaaf2d9
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2023-03-11 00:18:29 +0700

    Update job-screenshot-5s.ps1

--- installer/app/job-screenshot-5s.ps1
+++ installer/app/job-screenshot-5s.ps1
@@ -1,10 +1,15 @@
 
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(5) -RepetitionInterval (New-TimeSpan -Seconds 5) -RepetitionDuration ([TimeSpan]::MaxValue)
-$action = New-ScheduledTaskAction -Execute "python.exe" -Argument "-ExecutionPolicy Bypass -File C:\Program Files\screenshot.py"
-Register-ScheduledTask -TaskName "ScreenshotJob" -Trigger $trigger -Action $action -RunLevel Highest -User "administrator"
-
-
-
 $Action = New-ScheduledTaskAction -Execute "python.exe" -Argument "C:\Program Files\screenshot.py"
 Register-ScheduledJob -Name "ScreenshotJob" -ScriptBlock {python.exe 'C:\Program Files\screenshot.py'} -Trigger (New-JobTrigger -AtLogOn -RandomDelay 00:00:30)
 Register-ScheduledJob -Name "ScreenshotJob2" -ScriptBlock {python.exe 'C:\Program Files\screenshot.py'} -Trigger (New-JobTrigger -AtLogOn -User admin -RandomDelay 00:00:30)
+
+
+Set-ExecutionPolicy RemoteSigned
+$scriptPath = "C:\Program Files\screenshot.py"
+$ps1Path = "C:\Program Files\screenshot.ps1"
+Set-Content -Path $ps1Path -Value "python `"$scriptPath`""
+Register-ScheduledJob -Name "ScreenshotJob" -FilePath $ps1Path -Trigger (New-JobTrigger -AtLogOn)
+
+# Get-ScheduledJob
+# Unregister-ScheduledJob ScreenshotJob2
+
