9c8f294ab1c53876ee4ca053d49052682b1c5159
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2023-03-10 22:50:11 +0700

    Update job-screenshot-5s.ps1

--- installer/app/job-screenshot-5s.ps1
+++ installer/app/job-screenshot-5s.ps1
@@ -1,4 +1,17 @@
-$trigger = New-ScheduledJobTrigger -Once -At (Get-Date).AddSeconds(5) -RepetitionInterval (New-TimeSpan -Seconds 5) -RepetitionDuration ([TimeSpan]::MaxValue)
-$jobName = "ScreenshotJob"
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(5) -RepetitionInterval (New-TimeSpan -Seconds 5) -RepetitionDuration ([TimeSpan]::MaxValue)
+
+# $trigger = New-ScheduledJobTrigger -Once -At (Get-Date).AddSeconds(5) -RepetitionInterval (New-TimeSpan -Seconds 5) -RepetitionDuration ([TimeSpan]::MaxValue)
 $scriptPath = "C:\Program Files\screenshot.py"
-Register-ScheduledJob -Name $jobName -FilePath $scriptPath -Trigger $trigger
+# Register-ScheduledJob -Name "ScreenshotJob" -FilePath $scriptPath -Trigger $trigger
+Register-ScheduledTask -TaskName "ScreenshotJob" -Trigger $trigger -Action {Write-Host "Task is running"} -RunLevel Highest -User "SYSTEM"
+
+
+Register-ScheduledJob -Name "ScreenshotJob" -ScriptBlock {python.exe 'C:\Program Files\screenshot.py'} -Trigger (New-ScheduledJobTrigger -Once -At (Get-Date).AddSeconds(5) -RepetitionInterval (New-TimeSpan -Seconds 5) -RepetitionDuration ([TimeSpan]::MaxValue))
+
+$action = New-ScheduledTaskAction -Execute "python.exe" -Argument "-ExecutionPolicy Bypass -File C:\Program Files\screenshot.py"
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Seconds 5) -RepetitionDuration ([TimeSpan]::MaxValue)
+Register-ScheduledTask -TaskName "Screenshot" -Action $action -Trigger $trigger -User "administrator" -Password "D3v!@#0ps$%^"
+
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(5) -RepetitionInterval (New-TimeSpan -Seconds 5) -RepetitionDuration ([TimeSpan]::MaxValue)
+$action = New-ScheduledTaskAction -Execute "python.exe" -Argument "-ExecutionPolicy Bypass -File C:\Program Files\screenshot.py"
+Register-ScheduledTask -TaskName "ScreenshotJob" -Trigger $trigger -Action $action -RunLevel Highest -User "administrator"
