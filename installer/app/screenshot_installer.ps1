766a447318a64d6bdf7c2259454c01ac9ba790f1
BinhPhuong <phuong.lb@7-eleven.vn>
2023-03-12 21:35:40 +0700

    update screenshot

--- installer/app/screenshot_installer.ps1
+++ installer/app/screenshot_installer.ps1
@@ -23,8 +23,18 @@ Export-ScheduledTask -TaskName "ScreenshotJob" -TaskPath "\Path\To\Task" -Export
 # Import Job
 Import-ScheduledTask -TaskName "ScreenshotJob" -TaskPath "\Path\To\Task" -Xml (Get-Content "C:\ScheduledJob.xml" | Out-String) -User "Username" -Password "Password"
 
+#  Create job with nssm
+nssm.exe install "Screenshot" "C:\Python311\python.exe" "C:\Windows\System32\screenshot.py"
+C:\Users\data\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 
 # Create Job startup if not exits
 Start-Process powershell.exe -Verb runAs -ArgumentList '-noprofile -noexit -command "Set-ExecutionPolicy RemoteSigned"'
 $Settings = New-ScheduledJobOption -RunElevated -DoNotAllowDemandStart
-$Job = Register-ScheduledJob -Name "ScreenshotJob" -ScriptBlock { python 'C:\Windows\System32\screenshot.py' } -Trigger (New-JobTrigger -AtLogon -RandomDelay 00:00:15) -ScheduledJobOption $Settings
+$Job = Register-ScheduledJob -Name "Screenshot2" -ScriptBlock { python 'C:\Windows\System32\screenshot.py' } -Trigger (New-JobTrigger -AtLogon -RandomDelay 00:00:15) -ScheduledJobOption $Settings
+
+
+# Grand permit folder screens
+$Acl = Get-Acl -Path C:\Windows\screenshots\
+$Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("Users","FullControl","Allow")
+$Acl.SetAccessRule($Ar)
+Set-Acl -Path C:\Windows\screenshots\ -AclObject $Acl
