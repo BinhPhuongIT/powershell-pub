f2cb2a00fa31928cd94bd3fa9addd190ef0e0189
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2023-03-13 10:22:30 +0700

    Update screenshot_installer.ps1

--- installer/app/screenshot_installer.ps1
+++ installer/app/screenshot_installer.ps1
@@ -8,6 +8,8 @@
 # # vim
 # scoop install vim
 
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 # scoop install python
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install python
